bb3837e6e981f672d61cce99617b0c082afba86c
Wallace Goulart <wallacegc@hotmail.com>
2024-07-28 11:41:08 -0300

    exercicio de insecao de dados no MYSQL

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 import db.DB;
 
@@ -11,30 +15,60 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		Statement st = null;
-		ResultSet rs = null;
+		//Formatacao da Data 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		//Iniciando a conexao
+		Connection conn = null;
+		PreparedStatement st = null;
 		
 		try {
 			conn = DB.getConnection();
 			
-			st = conn.createStatement();
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+"(Name, Email , BirthDate , BaseSalary, DepartmentId) "
+					+"VALUES "
+					+"(? , ? , ? , ? , ?)" ,
+					Statement.RETURN_GENERATED_KEYS);
 			
-			rs = st.executeQuery("select * from department");
+			st.setString(1, "Carl Purple");
+			st.setString(2,"carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5,4);
 			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			int rowsAffected = st.executeUpdate();
+			
+			// Para retornar o id que foi inserido os dados
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()){
+					int id = rs.getInt(1);
+					System.out.println("Done! Id =  " + id);
+				}
+				
 			}
+			else {
+				System.out.println("No rows affected!!");
+			}
+
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
+		}// podemos ter data invalida
+		catch(ParseException e) {
+			e.printStackTrace();
 		}
 		finally {
+			DB.closeStatement(st);
 			DB.closeConnection();
-			DB.closeResultSet(rs); 
-			DB.closeStatement(st);;
-			
 		}
+		
+		
+		
+		
+
 		
 
 	}
